d0b94224be4e700a3edfc04450d455e5a4d1c35a
Soledine <soledine22@gmail.com>
2025-11-23 09:10:48 +0100

    FEAT: mise en place d'une mesure planifiee

--- src/main/java/fr/cnam/serverMonitor/MesureService.java
+++ src/main/java/fr/cnam/serverMonitor/MesureService.java
@@ -1,35 +1,30 @@
 package fr.cnam.serverMonitor;
 
+
 import fr.cnam.serverMonitor.entite.CpuMesure;
-import fr.cnam.serverMonitor.enumerations.TypeMesure;
 import fr.cnam.serverMonitor.repository.MesureCpuRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
 import oshi.SystemInfo;
 import oshi.hardware.CentralProcessor;
 
 import java.math.BigDecimal;
 import java.util.concurrent.TimeUnit;
 
-
-@SpringBootApplication
-@EnableScheduling
-public class ServerMonitorApplication implements CommandLineRunner {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ServerMonitorApplication.class, args);
-	}
+@Service
+public class MesureService {
 
     @Autowired
     private MesureCpuRepository mesureCpuRepository;
 
-    @Override
-    public void run(String... args) throws Exception {
+    public MesureService(MesureCpuRepository mesureCpuRepository) {
+        this.mesureCpuRepository = mesureCpuRepository;
+    }
 
-        /*
+    @Scheduled(fixedRate = 3000)
+    public void collectAndSaveMetrics() throws InterruptedException{
+
         CpuMesure mesure=new CpuMesure();
         SystemInfo si = new SystemInfo();
         CentralProcessor processor = si.getHardware().getProcessor();
@@ -38,7 +33,6 @@ public class ServerMonitorApplication implements CommandLineRunner {
         double cpuLoad = processor.getSystemCpuLoadBetweenTicks(prevTicks) * 100;
         mesure.setCpuLoad(new BigDecimal(cpuLoad));
         mesureCpuRepository.save(mesure);
-         */
+        System.out.println("nouvelle mesure en bdd : "+mesure);
     }
-
 }
